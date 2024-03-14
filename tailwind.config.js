6ea26b946e605c3064794ffdc6a19f8bf3ef5eea
chauhan piyush <143264181+piyush6028@users.noreply.github.com>
2024-03-14 11:22:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["dist/index.html"],
+  theme: {
+    fontFamily: {
+      nunito: ['Nunito'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
